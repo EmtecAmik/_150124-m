a091f6fe820abc0a98832394de43ba07b9582f44
EmtecAmic <romakrasov@gmail.com>
2024-02-20 10:14:09 +0100

    as

---
+++ src/main/java/org/example/_2024_02_19/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.example._2024_02_19;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");   // вывод в консоль аргументы
+    }
+}
