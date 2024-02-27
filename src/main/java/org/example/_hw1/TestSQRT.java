5d9618ce58229a3087a336ce64720e37ad54ff14
EmtecAmic <romakrasov@gmail.com>
2024-02-27 11:01:36 +0100

    as

---
+++ src/main/java/org/example/_hw1/TestSQRT.java
@@ -0,0 +1,76 @@
+//package org.example._hw1;
+//
+//public class TestSQRT {
+//
+//
+//
+////    // -площадь треугольника;
+//
+////    public static void main(String[] args) {
+////        int a = 10;
+////        int h = 5;
+////        double sqr = calculateTriangleSqr(a, h);
+////        System.out.println("Triangle square is " +sqr);
+////    }
+////
+////    private static double calculateTriangleSqr(int a, int h) {
+////        return (a * h) / 2.0;
+////    }
+//
+//
+//
+//
+//    // -объем куба;
+//
+////        public static void main(String[] args) {
+////            int a = 5;
+////            double volume = calculateCubeVolume(a);
+////            System.out.println("Cube volume is " + volume);
+////        }
+////
+////        private static double calculateCubeVolume(int a) {
+////            return Math.pow(a, 3);
+////        }
+//
+//
+//
+//
+//    //-периметр трапеции;
+//
+////    public static void main(String[] args) {
+////        double a = 3;
+////        double b = 5;
+////        double c = 4;
+////        double d = 6;
+////        double perimeter = calculateTrapezoidPerimeter(a, b, c, d);
+////        System.out.println("Trapezoid perimeter is " + perimeter);
+////    }
+////
+////    private static double calculateTrapezoidPerimeter(double a, double b, double c, double d) {
+////        return a + b + c + d;
+////    }
+//
+//
+//
+//
+////-площадь сферы.
+//
+//
+//    public static void main(String[] args) {
+//        double radius = 5;
+//        double area = calculateSphereArea(radius);
+//        System.out.println("Sphere area is " + area);
+//    }
+//
+//    private static double calculateSphereArea(double radius) {
+//        return 4 * Math.PI * Math.pow(radius, 2);
+//    }
+//
+//
+//}
+//
+//
+//
+//
+//
+//
